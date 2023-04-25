e2c311f109fb6c1c4533eb44829dca3ac95b934e
Rajib Deka <rajib.deka@siemens.com>
2023-04-25 10:57:40 +0530

    initial commit

---
+++ AddressBook/src/tutorial/adressbook/entity/Contact.java
@@ -0,0 +1,60 @@
+package tutorial.adressbook.entity;
+
+public class Contact {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int mobileNumber;
+    private String location;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(int mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", mobileNumber=" + mobileNumber +
+                ", location='" + location + '\'' +
+                '}';
+    }
+}
